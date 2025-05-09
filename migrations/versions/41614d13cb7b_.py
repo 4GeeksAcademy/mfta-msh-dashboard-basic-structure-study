274833d0cf52d3d0371513cc19bf34000d428460
Marta Fagúndez <55920614+MartaFagundez@users.noreply.github.com>
2025-05-09 01:45:07 +0000

    refactor: refactor User model

--- migrations/versions/41614d13cb7b_.py
+++ migrations/versions/41614d13cb7b_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 0763d677d453
+Revision ID: 41614d13cb7b
 Revises: 
-Create Date: 2025-02-25 14:47:16.337069
+Create Date: 2025-05-08 17:01:16.975891
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '0763d677d453'
+revision = '41614d13cb7b'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -21,10 +21,13 @@ def upgrade():
     op.create_table('user',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('email', sa.String(length=120), nullable=False),
-    sa.Column('password', sa.String(), nullable=False),
+    sa.Column('username', sa.String(length=80), nullable=False),
+    sa.Column('password', sa.String(length=300), nullable=False),
+    sa.Column('role', sa.Enum('ADMIN', 'USER', name='roleenum'), nullable=False),
     sa.Column('is_active', sa.Boolean(), nullable=False),
     sa.PrimaryKeyConstraint('id'),
-    sa.UniqueConstraint('email')
+    sa.UniqueConstraint('email'),
+    sa.UniqueConstraint('username')
     )
     # ### end Alembic commands ###
 
--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,36 @@
+import enum
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, Enum
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
+# role enum
+class RoleEnum(str, enum.Enum):
+    ADMIN = "admin"
+    USER = "user"
+
+    @classmethod
+    def get_choices(cls):
+        return [role.value for role in cls]
+
+
+# user model
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    username: Mapped[str] = mapped_column(String(80), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(300), nullable=False)
+    role: Mapped[enum] = mapped_column(Enum(RoleEnum), nullable=False, default=RoleEnum.USER.value)
+    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False, default=True)
 
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "username": self.username,
+            "role": self.role,
+            "is_active": self.is_active,
             # do not serialize the password, its a security breach
         }
